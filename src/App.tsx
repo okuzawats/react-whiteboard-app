262c9a80fac142b9b9b909e7e57c6266703b0d90
okuzawats <okuzawats@gmail.com>
2023-06-07 06:27:56 +0900

    make text draggable

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [pos, setPos] = React.useState({ x: 0, y: 0 });
   return (
-    <div>Thanks for all the fish.</div>
+    <div
+      style={{ width: "1000px", height: "1000px", position: "relative" }}
+      onDrop={ (e) => setPos({ x: e.clientX, y: e.clientY }) }
+      onDragOver={ (e) => e.preventDefault() }
+    >
+      <div style={{ position: "absolute", top: pos.y + "px", left: pos.x + "px" }} draggable={true}>
+        Thanks for all the fish.
+      </div>
+    </div>
   );
 }
 
