60cac38ace4d16817831cc1a0ed38d7bb636ac7b
okuzawats <okuzawats@gmail.com>
2023-06-07 22:29:10 +0900

    enable edit text area

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 
 function App() {
   const [pos, setPos] = React.useState({ x: 0, y: 0 });
+  const [text, setText] = React.useState("Thanks for all the fish.");
+  const [editMode, setEditMode] = React.useState(false);
+
   return (
     <div
       style={{ width: "1000px", height: "1000px", position: "relative" }}
@@ -10,7 +13,15 @@ function App() {
       onDragOver={ (e) => e.preventDefault() }
     >
       <div style={{ position: "absolute", top: pos.y + "px", left: pos.x + "px" }} draggable={true}>
-        Thanks for all the fish.
+        { editMode ? (
+          <textarea
+            onBlur={(e) => setEditMode(false)}
+            onChange={(e) => setText(e.target.value)}
+            defaultValue={text}
+          />
+        ) : (
+          <div onClick={(e) => setEditMode(true)}>{text}</div>
+        )}
       </div>
     </div>
   );
